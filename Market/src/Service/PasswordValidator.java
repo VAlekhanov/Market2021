9155a5bf32afacb5693ebbe5d80c87db364d4480
valekhanov <bitvull@gmail.com>
2021-10-18 06:31:29 +0400

    Password Validator

--- Market/src/Service/PasswordValidator.java
+++ Market/src/Service/PasswordValidator.java
@@ -1,6 +1,10 @@
 package Service;
 
 public class PasswordValidator {
-	
-	
+
+	public boolean isValid(String password) {
+		String pattern = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}";
+		return password.matches(pattern);
+	}
+
 }
